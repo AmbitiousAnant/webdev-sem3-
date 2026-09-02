ad8f7a47b897e6bd3ff80dc5ad8dd6539f8046db
AmbitiousAnant <anantthakkur19@gmail.com>
2026-09-02 09:46:10 +0530

    js revision

--- emitter.js
+++ emitter.js
@@ -1,7 +1,7 @@
 const EventEmitter = require('events'); 
 const myEmitter = new EventEmitter();
 myEmitter.on('greet', (name) => {
-    console.log(Hello, ${name}! Welcome to Node.js);
+    console.log(`Hello, ${name}Welcome to Node.js`);
 });
 myEmitter.on('exit', () => {
     console.log('Application Closed.');
